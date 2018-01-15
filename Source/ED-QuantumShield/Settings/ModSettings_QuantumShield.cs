1cdf5718d0ef77028e088cc976c6bc86d4997408
Joshua W <jaxxa90@gmail.com>
2018-01-15 18:09:06 +1100

    Setup of Settings

---
+++ Source/ED-QuantumShield/Settings/ModSettings_QuantumShield.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace ED_QuantumShield.Settings
+{
+    class ModSettings_ED_QuantumShield : ModSettings
+    {
+
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+
+//            Scribe_Values.Look<bool>(ref ShowLettersThreatBig, "ShowLettersThreatBig", true, true);
+
+
+        }
+
+
+        public void DoSettingsWindowContents(Rect canvas)
+        {
+            Listing_Standard listing_Standard = new Listing_Standard();
+            listing_Standard.ColumnWidth = 250f;
+            listing_Standard.Begin(canvas);
+            //listing_Standard.set_ColumnWidth(rect.get_width() - 4f);
+
+            listing_Standard.Label("Sections Starting with '*' only apply after Restart.");
+            listing_Standard.GapLine(12f);
+            listing_Standard.Label("Letter Suppression:");
+            listing_Standard.Gap(12f);
+
+            listing_Standard.End();
+        }
+    }
+}
+
---
+++ Source/ED-QuantumShield/Settings/Mod_QuantumShield.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace ED_QuantumShield.Settings
+{
+    class Mod_QuantumShield : Verse.Mod
+    {
+
+        public static ModSettings_ED_QuantumShield Settings;
+
+
+        public Mod_QuantumShield(ModContentPack content) : base(content)
+        {
+            Mod_QuantumShield.Settings = GetSettings<ModSettings_ED_QuantumShield>();
+        }
+
+        public override string SettingsCategory()
+        {
+            return "ED-QuantumShield";
+            //return base.SettingsCategory();
+        }
+
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Settings.DoSettingsWindowContents(inRect);
+            //base.DoSettingsWindowContents(inRect);
+        }
+
+    }
+}
