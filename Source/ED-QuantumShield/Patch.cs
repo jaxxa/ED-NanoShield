6b219282001620d85fb2fdc55219d1fa43885061
Joshua W <jaxxa90@gmail.com>
2018-02-04 14:53:22 +1100

    Code Refactoring

--- Source/ED-QuantumShield/Patch.cs
+++ Source/ED-QuantumShield/Patch.cs
@@ -18,56 +18,42 @@ namespace ED_QuantumShield
 
             HarmonyInstance _Harmony = HarmonyInstance.Create("EnhancedDevelopment.QuantumShield");
 
-            //Log.Message("QuantumShieldPatch");
-            //PawnKindDef _Def = PawnKindDefOf.Colonist;
-            //CompProperties_QuantumShield _CompProp = new CompProperties_QuantumShield();
-            //_Def.comps.Add(_CompProp);
-            //Log.Message("QuantumShieldPatch Done");
-
-
             Log.Message("#ED_QuantumShield.ApplyPatches() Starting");
 
-            //Get the Origional CheckSpring Method
-            //MethodInfo _PawnComponentsUtility_AddComponentsForSpawn = typeof(RimWorld.PawnComponentsUtility).GetMethod("AddComponentsForSpawn", BindingFlags.Public | BindingFlags.Static);
-
-            //MethodInfo _PawnComponentsUtility_AddComponentsForSpawn = typeof(Verse.Pawn).GetMethod("SpawnSetup", BindingFlags.Public | BindingFlags.Instance);
-            //Patch.LogNULL(_PawnComponentsUtility_AddComponentsForSpawn, "_PawnComponentsUtility_AddComponentsForSpawn", true);
-
-            MethodInfo _PawnComponentsUtility_AddComponentsForSpawn = typeof(Verse.ThingWithComps).GetMethod("InitializeComps", BindingFlags.Public | BindingFlags.Instance);
-            Patch.LogNULL(_PawnComponentsUtility_AddComponentsForSpawn, "_PawnComponentsUtility_AddComponentsForSpawn", true);
+            MethodInfo _ThingWithComps_InitializeComps = typeof(Verse.ThingWithComps).GetMethod("InitializeComps", BindingFlags.Public | BindingFlags.Instance);
+            Patch.LogNULL(_ThingWithComps_InitializeComps, "_ThingWithComps_InitializeComps", true);
 
             //Get the Prefix Patch
-            MethodInfo _AddCompPrefix = typeof(Patch).GetMethod("AddCompPrefix", BindingFlags.Public | BindingFlags.Static);
-            Patch.LogNULL(_AddCompPrefix, "_AddCompPrefix", true);
+            MethodInfo _InitializeCompsPrefix = typeof(Patch).GetMethod("InitializeCompsPrefix", BindingFlags.Public | BindingFlags.Static);
+            Patch.LogNULL(_InitializeCompsPrefix, "_AddCompPrefix", true);
 
             //Apply the Prefix Patch
-            _Harmony.Patch(_PawnComponentsUtility_AddComponentsForSpawn, new HarmonyMethod(_AddCompPrefix), null);
+            _Harmony.Patch(_ThingWithComps_InitializeComps, new HarmonyMethod(_InitializeCompsPrefix), null);
 
             Log.Message("#ED_QuantumShield.ApplyPatches() Completed");
         }
-
-
 
         // prefix
         // - wants instance, result and count
         // - wants to change count
         // - returns a boolean that controls if original is executed (true) or not (false)
-        public static bool AddCompPrefix(ThingWithComps __instance)
+        public static bool InitializeCompsPrefix(ThingWithComps __instance)
         {
+            //Bail out if not dealing with a Pawn
+            if (!(__instance is Pawn))
+            {
+                return true;
+            }
+
             CompProperties_QuantumShield _CompProp = new CompProperties_QuantumShield();
-            //Log.Message("DefName " + __instance.def.defName);
-            //string.Equals(__instance.def.thingClass, "Pawn") &&
-            
+
             if (__instance is Pawn && !__instance.def.comps.Any(x => string.Equals(x.compClass.FullName, _CompProp.compClass.FullName)))
             {
-            //Log.Message(__instance.def.comps.Count.ToString());
-            __instance.def.comps.Add(_CompProp);
+                __instance.def.comps.Add(_CompProp);
             }
-            //Log.Message(__instance.def.comps.Count.ToString());
 
             return true;
         }
-
 
         /// <summary>
         /// Debug Logging Helper
@@ -89,7 +75,5 @@ namespace ED_QuantumShield
                 }
             }
         }
-
     }
-
 }
